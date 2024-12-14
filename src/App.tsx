a3f82f254167940506f6aa557fa7fda307fe3c78
Lord_Evendim <24799163+LordEvendim@users.noreply.github.com>
2024-12-14 18:27:46 +0100

    feat: initial template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button, Text } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Text>Click Dziadek Assistant UI</Text>
+      <Button>Test</Button>
+    </>
+  );
+}
+
+export default App;
